d1ae479593d6bcd7e168f20e8cfc7289313b6cc5
Aurore-T <tellier.aurore@outlook.fr>
2026-04-07 16:14:51 +0200

    add cookie script

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,26 @@
 console.log('hello');
 
+document.cookie = "username=Oeschger";
+document.cookie = "email=email+1@xss.fr";
+document.cookie = "password=password";
+
+
+const main = document.querySelector('main');
+const a = document.createElement('a');
+
+a.textContent = 'Voir les établissements';
+a.href = 'https://developer.mozilla.org/fr/docs/Web/API/Document/createElement'
+a.target = '_blank';
+a.rel = 'noopener noreferrer';
+
+a.classList.add('btn', 'btn-danger');
+
+main.append(a);
+
+
+
+
+
 
 /** @info
  * A coller dans le input description pour la demo
